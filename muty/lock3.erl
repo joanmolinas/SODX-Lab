e030889e632e222031499130dd607fb677790213
Alex Carol <alex.carol.c@gmail.com>
2013-10-17 18:17:15 +0200

    Removed Id from lock3, as it was useless

--- muty/lock3.erl
+++ muty/lock3.erl
@@ -1,54 +1,54 @@
 -module(lock3).
 -export([init/2]).
 
-init(Id, Nodes) ->
-    open(Id, Nodes, 0).
-open(Id, Nodes, Time) ->
+init(_, Nodes) ->
+    open(Nodes, 0).
+open(Nodes, Time) ->
     receive
         {take, Master} ->
-            Refs = requests(Id, Nodes, Time),
-            wait(Id, Nodes, Master, Refs, [], Time, Time);
-        {request, From, _, Ref, SenderTime} ->
+            Refs = requests(Nodes, Time),
+            wait(Nodes, Master, Refs, [], Time, Time);
+        {request, From, Ref, SenderTime} ->
             CurrentTime = calculate_time(Time, SenderTime),
             From ! {ok, Ref, CurrentTime},
-            open(Id, Nodes, CurrentTime);
+            open(Nodes, CurrentTime);
         stop ->
             ok
     end.
 
 
-requests(Id, Nodes, Time) ->
+requests(Nodes, Time) ->
     lists:map(
         fun(P) ->
             R = make_ref(),
-            P ! {request, self(), Id, R, Time},
+            P ! {request, self(), R, Time},
             R
         end,
         Nodes).
 
-wait(Id, Nodes, Master, [], Waiting, Time, _) ->
+wait(Nodes, Master, [], Waiting, Time, _) ->
     Master ! taken,
-    held(Id, Nodes, Waiting, Time);
+    held(Nodes, Waiting, Time);
 
-wait(Id, Nodes, Master, Refs, Waiting, Time, RequestSentTime) ->
+wait(Nodes, Master, Refs, Waiting, Time, RequestSentTime) ->
     receive
-        {request, From, _, Ref, SenderTime} ->
+        {request, From, Ref, SenderTime} ->
             CurrentTime = calculate_time(Time, SenderTime),
             if
                 SenderTime < RequestSentTime ->
                     From ! {ok, Ref, CurrentTime},
-                    Refs2 = requests(Id, [From], CurrentTime),
-                    wait(Id, Nodes, Master, lists:merge(Refs, Refs2), Waiting, CurrentTime, RequestSentTime);
+                    Refs2 = requests([From], CurrentTime),
+                    wait(Nodes, Master, lists:merge(Refs, Refs2), Waiting, CurrentTime, RequestSentTime);
                 true ->
-                    wait(Id, Nodes, Master, Refs, [{From, Ref}|Waiting], CurrentTime, RequestSentTime)
+                    wait(Nodes, Master, Refs, [{From, Ref}|Waiting], CurrentTime, RequestSentTime)
             end;
         {ok, Ref, SenderTime} ->
             CurrentTime = calculate_time(Time, SenderTime),
             Refs2 = lists:delete(Ref, Refs),
-            wait(Id, Nodes, Master, Refs2, Waiting, CurrentTime, RequestSentTime);
+            wait(Nodes, Master, Refs2, Waiting, CurrentTime, RequestSentTime);
         release ->
             ok(Waiting, Time),
-            open(Id, Nodes, Time)
+            open(Nodes, Time)
     end.
 
 ok(Waiting, Time) ->
@@ -58,14 +58,14 @@ ok(Waiting, Time) ->
         end,
         Waiting).
 
-held(Id, Nodes, Waiting, Time) ->
+held(Nodes, Waiting, Time) ->
     receive
-        {request, From, _, Ref, SenderTime} ->
+        {request, From, Ref, SenderTime} ->
             CurrentTime = calculate_time(Time, SenderTime),
-            held(Id, Nodes, [{From, Ref}|Waiting], CurrentTime);
+            held(Nodes, [{From, Ref}|Waiting], CurrentTime);
         release ->
             ok(Waiting, Time),
-            open(Id, Nodes, Time)
+            open(Nodes, Time)
     end.
 
 calculate_time(Time, SenderTime) -> lists:max([Time, SenderTime]) + 1.
