cc50e0fdf28fba9eb3af03bba929f99ca38b99c7
Alex Carol <alex.carol.c@gmail.com>
2013-11-14 18:04:30 +0100

    Revert "Splitted paxy so that proposers and acceptors are started on different nodes"
    This reverts commit c7831805996257204e2b10388e9b3bc50ad50cad.

--- paxy/paxy.erl
+++ paxy/paxy.erl
@@ -1,46 +1,36 @@
 -module(paxy).
--export([start_acceptor_node/2, start_proposer_node/0, stop/0, stop/1]).
+-export([start/1, stop/0, stop/1]).
 -define(RED, {255,0,0}).
 -define(GREEN, {0,255,0}).
 -define(BLUE, {0,0,255}).
 -define(MAGENTA, {0, 255, 255}).
 -define(CYAN, {255, 0, 255}).
 
-start_acceptor_node(Seed, ProposerId) ->
-  AcceptorNames = ["Acceptor 1", "Acceptor 2", "Acceptor 3","Acceptor 4", "Acceptor 5", "Acceptor 6", "Acceptor 7"],
-  AccRegister = [a, b, c, d, e, f, g],
-  ProposerNames = ["Proposer 1", "Proposer 2", "Proposer 3", "Proposer 4", "Proposer 5"],
-  PropInfo = [{kurtz, ?RED, 10}, {kilgore, ?GREEN, 2}, {willard, ?BLUE, 3}, {bill, ?CYAN, 21}, {zorro, ?MAGENTA, 32}],
-  % computing panel heights
-  AccPanelHeight = length(AcceptorNames)*50 + 20, %plus the spacer value
-  PropPanelHeight = length(ProposerNames)*50 + 20,
-  register(gui, spawn(fun() -> gui:start(AcceptorNames, ProposerNames, AccPanelHeight, PropPanelHeight) end)),
-  gui ! {reqState, self()},
-  receive
-    {reqState, State} ->
-      {AccIds, PropIds} = State,
-      start_acceptors(AccIds, AccRegister, Seed),
-      AccRegister2 = lists:map(fun(Name) -> {Name, node()} end, AccRegister),
-      ProposerId ! {startprops, PropIds, PropInfo, AccRegister2, Seed}
-  end,
-  true.
-
-start_proposer_node() ->
-  register(proposers, self()),
-  receive
-    {startprops, PropIds, PropInfo, AccRegister, Seed} ->
-      start_proposers(PropIds, PropInfo, AccRegister, Seed+1)
-  end,
-  true.
-
+start(Seed) ->
+    AcceptorNames = ["Acceptor 1", "Acceptor 2", "Acceptor 3","Acceptor 4", "Acceptor 5", "Acceptor 6", "Acceptor 7"],
+    AccRegister = [a, b, c, d, e, f, g],
+    ProposerNames = ["Proposer 1", "Proposer 2", "Proposer 3", "Proposer 4", "Proposer 5"],
+    PropInfo = [{kurtz, ?RED, 10}, {kilgore, ?GREEN, 2}, {willard, ?BLUE, 3}, {bill, ?CYAN, 21}, {zorro, ?MAGENTA, 32}],
+    % computing panel heights
+    AccPanelHeight = length(AcceptorNames)*50 + 20, %plus the spacer value
+    PropPanelHeight = length(ProposerNames)*50 + 20,
+    register(gui, spawn(fun() -> gui:start(AcceptorNames, ProposerNames,
+    AccPanelHeight, PropPanelHeight) end)),
+    gui ! {reqState, self()},
+    receive
+        {reqState, State} ->
+            {AccIds, PropIds} = State,
+            start_acceptors(AccIds, AccRegister, Seed),
+            start_proposers(PropIds, PropInfo, AccRegister, Seed+1)
+    end,
+    true.
 start_acceptors(AccIds, AccReg, Seed) ->
     case AccIds of
         [] ->
             ok;
         [AccId|Rest] ->
             [RegName|RegNameRest] = AccReg,
-            AcceptorPid = acceptor:start(RegName, Seed, AccId),
-            register(RegName, AcceptorPid),
+            register(RegName, acceptor:start(RegName, Seed, AccId)),
             start_acceptors(Rest, RegNameRest, Seed+1)
     end.
 start_proposers(PropIds, PropInfo, Acceptors, Seed) ->
