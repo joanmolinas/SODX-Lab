9a5f4f5d6b3d3fa25d040d6af7bdc21a003339d7
Alex Carol <alex.carol.c@gmail.com>
2013-12-03 01:50:23 +0100

    Implemented list randomizer

--- opty/server2.erl
+++ opty/server2.erl
@@ -6,22 +6,21 @@ start(N, NumRandomEntries) ->
 init(N, NumRandomEntries) ->
   Store = store:new(N),
   Validator = validator:start(),
-  server(Validator, Store, NumRandomEntries).
-server(Validator, Store, NumRandomEntries) ->
+  server(Validator, Store, NumRandomEntries, N).
+server(Validator, Store, NumRandomEntries, StoreSize) ->
   receive
     {open, Client} ->
-      RandomizedStoreSubset = getRandomSubset(Store, NumRandomEntries),
+      RandomizedStoreSubset = getRandomSubset(Store, NumRandomEntries, StoreSize),
       Client ! {transaction, Validator, RandomizedStoreSubset},
-      server(Validator, Store, NumRandomEntries);
+      server(Validator, Store, NumRandomEntries, StoreSize);
     stop ->
       store:stop(Store)
   end.
 
-getRandomSubset(Tuple, NumRandomEntries) when is_tuple(Tuple) ->
-  list_to_tuple(getRandomSubset(tuple_to_list(Tuple), NumRandomEntries));
-getRandomSubset(List, NumRandomEntries) when is_list(List) ->
-  random:seed(now()),
-  lists:sublist(
-    [X||{_,X} <- lists:sort([{random:uniform(), Elem} || Elem <- List])],
-    NumRandomEntries
-  ).
+getRandomSubset(Tuple, StoreSize, StoreSize) ->
+  Tuple;
+getRandomSubset(Tuple, NumRandomEntries, StoreSize) when is_tuple(Tuple) ->
+  list_to_tuple(getRandomSubset(tuple_to_list(Tuple), NumRandomEntries, StoreSize));
+getRandomSubset(List, NumRandomEntries, StoreSize) when is_list(List) ->
+  Start = random:uniform(StoreSize - NumRandomEntries),
+  lists:sublist(List, Start, NumRandomEntries).
